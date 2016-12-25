68587680d97c98f48ab8fc7c6d5cdda236e2b9c3
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-12-25 20:54:46 +0100

    start confirmation view

--- register/views.py
+++ register/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
+from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from django.views import View
+from django.views.generic import TemplateView
 from register import models, serializers
 from register.forms import TypeformFetcher
 from rest_framework import generics
@@ -9,18 +11,33 @@ from rest_framework import generics
 class ApplicationListView(generics.ListAPIView):
     queryset = models.Application.objects.all()
     serializer_class = serializers.ApplicationsSerializer
+    renderer_classes = (renderers.AdminRenderer, renderers.JSONRenderer,)
 
 
 class UpdateApplications(View):
     def get(self, request):
         return HttpResponse(TypeformFetcher().update_forms())
 
-# TODO: Set actions to POST
-class ConfirmApplication(View):
-    def get(self, request, token):
-        application = models.Application.objects.get(id=token)
-        application.confirm()
-        return HttpResponse('CONFIRMED')
+
+class ConfirmApplication(TemplateView):
+    template_name = 'confirm.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ConfirmApplication, self).get_context_data(**kwargs)
+        application = models.Application.objects.get(id=context['token'])
+        request = self.request
+        try:
+            application.confirm()
+            context.update({
+                'application': application,
+                'cancel': application.cancelation_url(request)
+            })
+        except ValidationError:
+            context.update({
+                'error': "application can't be confirmed",
+            })
+
+        return context
 
 
 class InviteApplication(View):
