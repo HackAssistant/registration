74ecb5b226bfbc89b2cd18f3bfab6c3b84e4082e
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-12-25 23:43:24 +0100

    add static root

--- app/settings.py
+++ app/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR+'/staticfiles'
 
 EMAIL_BACKEND = "sgbackend.SendGridBackend"
 SENDGRID_API_KEY = os.environ.get('SG_KEY','.')
