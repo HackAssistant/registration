5841d76fb7597de1a17ec060f1cc373f41aea630
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-12-28 18:13:08 +0100

    fix voting login and refactor obtain next application to vote

--- register/views.py
+++ register/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
 from __future__ import print_function
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ValidationError
+from django.db.models import Count
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.views import View
@@ -14,29 +16,28 @@ class UpdateApplications(View):
     def get(self, request):
         return HttpResponse(ApplicationsTypeform().update_forms())
 
+
 class VoteApplicationView(LoginRequiredMixin, TemplateView):
-
-    login_url = '/accounts/login/'
-    redirect_field_name = '/vote/'
-
     template_name = 'app_vote.html'
 
-
     def get_user_unvotted_applications(self):
-        votes = list(models.Vote.objects.all())
-        applications = list(models.Application.objects.all())
-        for vote in votes:
-            if (vote.user_id == self.request.user.id):
-                applications.remove(vote.application)
-        print(len(applications))
-        return applications#sorted(applications, key=votes)
+        """
+        Django model to the rescue. This is transformed to an SQL sentence that does exactly what we need
+        :return: pending aplication that has not been voted by the current user and that has less votes and its older
+        """
+        return models.Application.objects \
+            .exclude(vote__user_id=self.request.user.id) \
+            .filter(status='P') \
+            .annotate(count=Count('vote')) \
+            .order_by('count', 'submission_date') \
+            .first()
 
     def post(self, request, *args, **kwargs):
         if request.POST.get('Accept'):
             v = models.Vote()
             v.user_id = request.user.id
-            applications = models.Application.objects.all()
-            a = applications.first()
+            applications = self.get_user_unvotted_applications()
+            a = applications
             v.application = a
             v.vote = 1
             v.save()
@@ -62,17 +63,17 @@ class VoteApplicationView(LoginRequiredMixin, TemplateView):
         return HttpResponseRedirect('/vote')
 
     def get_context_data(self, **kwargs):
-        print ("asdasdasd")
+        print("asdasdasd")
         context = super(VoteApplicationView, self).get_context_data(**kwargs)
         applications = self.get_user_unvotted_applications()
 
-        if len(applications) == 0:
+        if not applications:
             print("No applications")
             self.template_name = 'no_applications_left.html'
             context["title"] = "There are no applications to vote."
             return context
 
-        a = applications[0]
+        a = applications
         context["name"] = a.name
         context["from"] = a.country
         context["university"] = a.university
@@ -81,6 +82,7 @@ class VoteApplicationView(LoginRequiredMixin, TemplateView):
         context["dp_image_src"] = "https://maxcdn.icons8.com/Share/icon/ios7/Logos//devpost1600.png"
         context["github_image_src"] = "https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png"
         return context
+
 
 class ConfirmApplication(TemplateView):
     template_name = 'confirm.html'
@@ -99,7 +101,6 @@ class ConfirmApplication(TemplateView):
             context.update({
                 'error': "application can't be confirmed",
             })
-
 
         return context
 
