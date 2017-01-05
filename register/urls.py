adbd4b94a802a15428762a26e640a694a044969a
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2017-01-05 14:39:52 +0100

    add link to admin if admin

--- register/urls.py
+++ register/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'applications/(?P<token>\w+)/confirm$', views.ConfirmApplication.as_view(), name='confirm_app'),
     url(r'applications/(?P<token>\w+)/cancel$', views.CancelApplication.as_view(), name='cancel_app'),
     url(r'vote/$', views.VoteApplicationView.as_view(), name='vote'),
-    url(r'$', views.root_view, name='root')
+    url(r'^$', views.root_view, name='root')
 ]
