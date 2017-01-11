5a78304d9c094aee208133accdc9c9ffa17ea1f0
Pau Oliver <pau.oliver@est.fib.upc.edu>
2017-01-11 17:06:17 +0100

    Use django settings properly, small code clean-ups

--- register/emails.py
+++ register/emails.py
@@ -1,8 +1,8 @@
 import json
 
 from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
 import sendgrid
-from app.settings import SENDGRID_API_KEY
 
 
 def sendgrid_send(recipients, subject, substitutions, template_id):
@@ -27,9 +27,9 @@ class MailListManager:
     WINTER_17_LIST_ID = "876178"
 
     def __init__(self):
-        self.sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
+        self.sg = sendgrid.SendGridAPIClient(apikey=settings.SENDGRID_API_KEY)
 
-    def add_recipient_to_list(self, recipient_id, list_id):
+    def _add_recipient_to_list(self, recipient_id, list_id):
         response = self.sg.client.contactdb.lists._(list_id).recipients._(recipient_id).post()
         if response.status_code != 201:
             raise ConnectionError  # TODO: raise an appropriate exception/error
@@ -60,13 +60,12 @@ class MailListManager:
             application.save()
 
         # Add recipient to list
-        self.add_recipient_to_list(recipient_id, list_id)
+        self._add_recipient_to_list(recipient_id, list_id)
 
     def remove_recipient_from_list(self, recipient_id, list_id):
         params = {'recipient_id': recipient_id, 'list_id': list_id}
         lists = self.sg.client.contactdb.lists
         response = lists._(list_id).recipients._(recipient_id).delete(query_params=params)
 
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
+        if response.status_code != 201:
+            raise ConnectionError
