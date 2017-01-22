7c4276c9161c20cede4a92d76894f78335c9b19a
Álvaro Girona Arias <alvaro.girona@gmail.com>
2017-01-22 03:36:48 +0100

    Templates directory on checkin.

--- checkin/views.py
+++ checkin/views.py
@@ -6,20 +6,25 @@ from django.db import models
 # Create your views here.
 from django.views.generic import TemplateView
 from models import CheckIn
+
 from register.models import Application
+
 
 def checkInHacker(application):
     checkin = CheckIn()
     checkin.application = application
     checkin.save()
 
+
 def getNotCheckedInhackersList():
     hackersList = Application.objects.exclude(id__in=[checkin.application for checkin in CheckIn.objects.all()])
     return hackersList
+
 
 class CheckInList(LoginRequiredMixin, TemplateView):
     template_name = 'templates/check-in-list.html'
     hackersList = getNotCheckedInhackersList()
 
+
 class CheckInHacker(LoginRequiredMixin, TemplateView):
-    template_name = 'check-in-hacker.hmtl'
+    template_name = 'templates/check-in-hacker.html'
