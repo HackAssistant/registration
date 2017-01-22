67737f2e451698ce6cdf84239096f7a165dba0e4
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2017-01-22 19:56:35 +0100

    add standarization to votes

--- register/models.py
+++ register/models.py
@@ -7,6 +7,10 @@ from django.db.models import Avg, F
 from register.emails import sendgrid_send, MailListManager
 from register.utils import reverse
 
+TECH_WEIGHT = 0.2
+
+PERSONAL_WEIGHT = 0.8
+
 APP_ACCEPTED = 'A'
 APP_PENDING = 'P'
 APP_REJECTED = 'R'
@@ -65,7 +69,6 @@ class Application(models.Model):
     # Needs to be set to true -> else rejected
     authorized_mlh = models.NullBooleanField()
     status = models.CharField(choices=STATUS, default=APP_PENDING, max_length=2)
-
 
     # TODO: TEAM EXTERNAL
 
@@ -152,6 +155,7 @@ VOTES = (
     (10, '10'),
 )
 
+
 class Vote(models.Model):
     application = models.ForeignKey(Application)
     user = models.ForeignKey(admin_models.User)
@@ -161,12 +165,39 @@ class Vote(models.Model):
 
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
+        """
+        We are overriding this in order to standarize each review vote with the new vote.
+        Also we store a calculated vote for each vote so that we don't need to do it later.
+
+        Thanks to Django awesomeness we do all the calculations with only 3 queries to the database.
+        2 selects and 1 update. The performance is way faster than I thought. If improvements need to be done
+        using a better DB than SQLite should increase performance. As long as the database can handle aggregations
+        efficiently this will be good.
+
+        By casassg
+        """
         super(Vote, self).save(force_insert, force_update, using, update_fields)
-        avgs = admin_models.User.objects.filter(id=self.user_id).aggregate(t_avg=Avg('vote__tech'),
-                                                                           p_avg=Avg('vote__personal'))
+        # Retrieve averages
+        avgs = admin_models.User.objects.filter(id=self.user_id).aggregate(tech=Avg('vote__tech'),
+                                                                           pers=Avg('vote__personal'))
+        p_avg = round(avgs['pers'], 2)
+        t_avg = round(avgs['tech'], 2)
+
+        # Calculate standard deviation for each scores
+        sds = admin_models.User.objects.filter(id=self.user_id).aggregate(
+            tech=Avg((F('vote__tech') - t_avg) * (F('vote__tech') - t_avg)),
+            pers=Avg((F('vote__personal') - p_avg) * (F('vote__personal') - p_avg)))
+        p_sd = round(sds['pers'], 2)
+        t_sd = round(sds['tech'], 2)
+
+        # Apply standarization. Standarization formula:
+        # x(new) = (x - u)/o
+        # where u is the mean and o is the standard deviation
+        #
+        # See this: http://www.dataminingblog.com/standardization-vs-normalization/
         Vote.objects.filter(user=self.user).update(
-            calculated_vote=0.8 * (F('personal') - round(avgs['p_avg'], 2)) + 0.2 * (
-            F('tech') - round(avgs['t_avg'], 2)))
+            calculated_vote=PERSONAL_WEIGHT * (F('personal') - p_avg) / p_sd + TECH_WEIGHT * (
+                F('tech') - t_avg) / t_sd)
 
     class Meta:
         unique_together = ('application', 'user')
