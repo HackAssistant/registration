75c02dc82a3c1780a31a240db82fee120446fa6c
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2017-01-23 03:44:47 +0100

    add documentation regarding deployment

---
+++ restart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd /home/deploy/backend/
+echo "checking updates..."
+./env/bin/pip install -r requirements.txt
+echo "checking updates...done"
+echo "migrating db..."
+./env/bin/python manage.py migrate
+echo "migrating db...done"
+echo "collecting static..."
+./env/bin/python manage.py collectstatic --no-input
+echo "collecting static...done"
+echo "Deploy completed. The game is on!"
