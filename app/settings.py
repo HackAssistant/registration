0ad89b5a989757e39c473b35aae2d5d3af42d7dd
Gerard Casas Saez <casassg@gmail.com>
2017-10-24 21:15:28 -0600

    fix error on python3

--- app/settings.py
+++ app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
-from hackathon_variables import *
+from .hackathon_variables import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
