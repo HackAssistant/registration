2391da99ceb07153e0932c89a68b996c23b5a8f6
Gerard Casas Saez <casassg@gmail.com>
2017-11-01 16:36:50 -0600

    add fields to table

--- organizers/tables.py
+++ organizers/tables.py
@@ -10,17 +10,18 @@ class ApplicationFilter(django_filters.FilterSet):
 
     class Meta:
         model = Application
-        fields = ['user__email', 'user__nickname', 'status']
+        fields = ['user__email', 'user__nickname', 'status', 'first_timer', 'scholarship']
 
 
 class ApplicationsListTable(tables.Table):
     detail = tables.TemplateColumn(
         "<a href='{% url 'app_detail' record.uuid %}' target='_blank' class='btn btn-default'>Detail</a> ",
-        verbose_name='Actions', )
+        verbose_name='Actions', orderable=False)
 
     class Meta:
         model = Application
         attrs = {'class': 'table table-hover'}
         template = 'django_tables2/bootstrap-responsive.html'
-        fields = ['user.nickname', 'vote_avg', 'user.email', 'status']
+        fields = ['user.nickname', 'vote_avg', 'user.email', 'status', 'reimbursement.assigned_money']
         empty_text = 'No applications available'
+        order_by = '-vote_avg'
