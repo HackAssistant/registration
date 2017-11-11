cea968700e76616c3180c6ae38c247e2d79ca7c9
Gerard Casas Saez <casassg@gmail.com>
2017-11-10 21:55:16 -0700

    fix countdown for reimbursement

--- reimbursement/emails.py
+++ reimbursement/emails.py
@@ -3,13 +3,13 @@ from app.utils import reverse
 
 
 def create_reimbursement_email(reimb, request):
-    app = reimb.application
+    app = reimb.hacker.application
     c = {
         'app': app,
         'reimb': reimb,
-        'confirm_url': str(reverse('confirm_app', request=request)),
-        'form_url': reimb.get_form_url(),
-        'cancel_url': str(reverse('cancel_app', request=request))
+        'confirm_url': str(reverse('confirm_app', kwargs={'id': app.uuid_str}, request=request)),
+        'form_url': str(reverse('dashboard', request=request)),
+        'cancel_url': str(reverse('cancel_app', kwargs={'id': app.uuid_str}, request=request))
     }
     return emails.render_mail('mails/reimbursement',
-                              app.user.email, c)
+                              reimb.hacker.email, c)
