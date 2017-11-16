d6bca5826b49c5392d60df40b0aaaa3da901342b
Gerard Casas Saez <casassg@gmail.com>
2017-11-16 13:30:03 -0700

    refactor emails reimbursement

--- reimbursement/emails.py
+++ reimbursement/emails.py
@@ -4,18 +4,17 @@ from app.utils import reverse
 
 def create_reimbursement_email(reimb, request):
     app = reimb.hacker.application
-    c = {
-        'app': app,
-        'reimb': reimb,
-        'confirm_url': str(reverse('confirm_app', kwargs={'id': app.uuid_str}, request=request)),
-        'form_url': str(reverse('reimbursement_form', request=request)),
-        'cancel_url': str(reverse('cancel_app', kwargs={'id': app.uuid_str}, request=request))
-    }
+    c = _get_context(app, reimb, request)
     return emails.render_mail('mails/reimbursement', reimb.hacker.email, c)
 
 
 def create_reject_receipt_email(reimb, request):
     app = reimb.hacker.application
+    c = _get_context(app, reimb, request)
+    return emails.render_mail('mails/reject_receipt', reimb.hacker.email, c, from_email=reimb.reimbursed_by.email)
+
+
+def _get_context(app, reimb, request):
     c = {
         'app': app,
         'reimb': reimb,
@@ -23,4 +22,4 @@ def create_reject_receipt_email(reimb, request):
         'form_url': str(reverse('reimbursement_form', request=request)),
         'cancel_url': str(reverse('cancel_app', kwargs={'id': app.uuid_str}, request=request))
     }
-    return emails.render_mail('mails/reject_receipt', reimb.hacker.email, c, from_email=reimb.reimbursed_by.email)
+    return c
