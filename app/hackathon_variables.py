0a1d7c8b0c3dca84c5c1dd5c34a1d80cd663eb20
Gerard Casas Saez <casassg@gmail.com>
2017-11-20 02:06:14 -0700

    personalize for HackUPC

--- app/hackathon_variables.py
+++ app/hackathon_variables.py
@@ -3,30 +3,29 @@ import os
 
 from django.utils import timezone
 
-HACKATHON_NAME = 'HackAssistant'
+HACKATHON_NAME = 'HackUPC'
 # What's the name for the application
-HACKATHON_APPLICATION_NAME = 'HackAssistant registration'
+HACKATHON_APPLICATION_NAME = 'My HackUPC'
 # Hackathon timezone
-TIME_ZONE = 'MST'
+TIME_ZONE = 'CET'
 # This description will be used on the html and sharing meta tags
-HACKATHON_DESCRIPTION = 'HackAssistant is an organization to mantain ' \
-                        'a few open-source projects related with hackathon management'
+HACKATHON_DESCRIPTION = 'Join us for BarcelonaTech\'s hackathon. 700 hackers. 36h. October 13th-15th.'
 # Domain where application is deployed, can be set by env variable
 HACKATHON_DOMAIN = os.environ.get('DOMAIN', 'localhost:8000')
 # Hackathon contact email: where should all hackers contact you. It will also be used as a sender for all emails
-HACKATHON_CONTACT_EMAIL = 'contact@gerard.space'
+HACKATHON_CONTACT_EMAIL = 'contact@hackupc.com'
 # Hackathon logo url, will be used on all emails
-HACKATHON_LOGO_URL = 'https://hackcu.org/img/hackcu_black.png'
+HACKATHON_LOGO_URL = 'https://my.hackupc.com/static/logo.png'
 
 HACKATHON_OG_IMAGE = 'https://hackcu.org/img/hackcu_ogimage870x442.png'
 # (OPTIONAL) Track visits on your website
-# HACKATHON_GOOGLE_ANALYTICS = 'UA-7777777-2'
+HACKATHON_GOOGLE_ANALYTICS = 'UA-69542332-2'
 # (OPTIONAL) Hackathon twitter user
-HACKATHON_TWITTER_ACCOUNT = 'casassaez'
+HACKATHON_TWITTER_ACCOUNT = 'hackupc'
 # (OPTIONAL) Hackathon Facebook page
-HACKATHON_FACEBOOK_PAGE = 'casassaez'
+HACKATHON_FACEBOOK_PAGE = 'hackupc'
 # (OPTIONAL) Github Repo for this project (so meta)
-HACKATHON_GITHUB_REPO = 'https://github.com/hackassistant/registration/'
+HACKATHON_GITHUB_REPO = 'https://github.com/hackupc/registration/'
 
 # (OPTIONAL) Applications deadline
 # HACKATHON_APP_DEADLINE = timezone.datetime(2018, 2, 24, 3, 14, tzinfo=timezone.pytz.timezone(TIME_ZONE))
@@ -35,20 +34,20 @@ HACKATHON_GITHUB_REPO = 'https://github.com/hackassistant/registration/'
 #                    'the opening ceremony will be at 7:00 pm.'
 
 # (OPTIONAL) When to arrive at the hackathon
-# HACKATHON_LEAVE = 'Closing ceremony will be held on Sunday October 15th from 3:00 PM to 5:00 PM. ' \
-#                   'However the projects demo fair will be held in the morning from 10:30 AM to 1 PM.'
+HACKATHON_LEAVE = 'Closing ceremony will be held on Sunday October 15th from 3:00 PM to 5:00 PM. ' \
+                  'However the projects demo fair will be held in the morning from 10:30 AM to 1 PM.'
 # (OPTIONAL) Hackathon live page
-# HACKATHON_LIVE_PAGE = 'https://gerard.space/live'
+HACKATHON_LIVE_PAGE = 'https://hackupc.com/live'
 
 # (OPTIONAL) Regex to automatically match organizers emails and set them as organizers when signing up
-REGEX_HACKATHON_ORGANIZER_EMAIL = '^.*@gerard\.space$'
+REGEX_HACKATHON_ORGANIZER_EMAIL = '^.*@hackupc\.com$'
 # (OPTIONAL) Send 500 errors to email while on production mode
-# HACKATHON_DEV_EMAILS = ['contact@gerard.space', ]
+HACKATHON_DEV_EMAILS = ['devs@hackupc.com', ]
 
 # Reimbursement configuration
 REIMBURSEMENT_ENABLED = True
 DEFAULT_REIMBURSEMENT_AMOUNT = 100
-CURRENCY = '$'
+CURRENCY = '€'
 REIMBURSEMENT_EXPIRACY_DAYS = 5
 REIMBURSEMENT_REQUIREMENTS = 'You have to submit a project and demo it during the event in order to get reimbursed'
 
