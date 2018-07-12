28a9cdf9b31421a6e8fcef37506538066fb21306
Oriol Closa Márquez <oriol20@gmail.com>
2018-07-12 20:48:39 +0200

    Non ASCII character... Pfff...

--- user/forms.py
+++ user/forms.py
@@ -37,7 +37,7 @@ class RegisterForm(LoginForm):
     name = forms.CharField(label='Full name', max_length=225, help_text='How do you want us to call you?')
 
     terms_and_conditions = forms.BooleanField(
-        label='I’ve read, understand and accept <a href="/privacy_and_cookies" target="_blank">HackUPC '
+        label='I\'ve read, understand and accept <a href="/privacy_and_cookies" target="_blank">HackUPC '
               'Privacy and Cookies Policy</a>.<span style="color: red; font-weight: bold;"> *</span>')
 
     field_order = ['name', 'email', 'password', 'password2', 'terms_and_conditions']
