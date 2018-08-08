091b4b7fd34eb240dcdcb27f9f624653185d6807
oriolclosa <oriol.closa@est.fib.upc.edu>
2018-08-09 01:14:51 +0200

    Fixture and foreign key show on admin

--- baggage/views.py
+++ baggage/views.py
@@ -1,21 +1,22 @@
 from django.core.urlresolvers import reverse
 from app.mixins import TabsViewMixin
-from django.views.generic import TemplateView
-from baggage.tables import BaggageListTable
-from baggage.models import Item
+from baggage.tables import BaggageListTable, BaggageListFilter
+from baggage.models import Bag
 from django_tables2 import SingleTableMixin
+from django_filters.views import FilterView
 
 def organizer_tabs(user):
-    t = [('Check-in', reverse('baggage_list'), False)]
+    t = [('List', reverse('baggage_list'), False)]
     return t
 
-class BaggageList(TabsViewMixin, SingleTableMixin, TemplateView):
+class BaggageList(TabsViewMixin, SingleTableMixin, FilterView):
     template_name = 'baggage_list.html'
     table_class = BaggageListTable
+    filterset_class = BaggageListFilter
     table_pagination = {'per_page': 100}
   
     def get_current_tabs(self):
         return organizer_tabs(self.request.user)
     
     def get_queryset(self):
-        return Item.objects.all()
+        return Bag.objects.filter(active=True)
