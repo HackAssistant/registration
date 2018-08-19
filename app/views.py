fe736d8e31143259a1dae73374526702b03c9694
oriolclosa <oriol.closa@est.fib.upc.edu>
2018-08-19 10:35:35 +0200

    Fixed parenthesis

--- app/views.py
+++ app/views.py
@@ -48,12 +48,12 @@ def protectedMedia(request, file_):
     if path == "resumes":
         app = get_object_or_404(Application, resume=file_)
         if request.user.is_authenticated() and (request.user.is_organizer or
-                                           (app and (app.user_id == request.user.id))):
+                                                (app and (app.user_id == request.user.id))):
             downloadable_path = app.resume.path
     elif path == "receipt":
         app = get_object_or_404(Reimbursement, receipt=file_)
         if request.user.is_authenticated() and (request.user.is_organizer or
-                                           (app and (app.hacker_id == request.user.id))):
+                                                (app and (app.hacker_id == request.user.id))):
             downloadable_path = app.receipt.path
     if downloadable_path:
         response = StreamingHttpResponse(open(downloadable_path, 'rb'))
