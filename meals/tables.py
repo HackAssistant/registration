4b377ba1073ddea42897420117578b106f40bc4c
oriolclosa <oriol.closa@est.fib.upc.edu>
2018-09-26 23:47:15 +0200

    Change date format

--- meals/tables.py
+++ meals/tables.py
@@ -20,13 +20,15 @@ class MealsListTable(tables.Table):
     change = tables.TemplateColumn(
         "<a href='{% url 'meal_detail' record.id %}'>Modify</a> ",
         verbose_name='Actions', orderable=False)
+    starts2 = tables.DateTimeColumn(accessor='starts', verbose_name='Starts', format='d/m/Y G:i')
+    ends2 = tables.DateTimeColumn(accessor='ends', verbose_name='Ends', format='d/m/Y G:i')
     eaten = tables.Column(accessor='eaten', verbose_name='Eaten')
 
     class Meta:
         model = Meal
         attrs = {'class': 'table table-hover'}
         template = 'templates/meals_list.html'
-        fields = ['id', 'name', 'type', 'times', 'starts', 'ends', 'eaten']
+        fields = ['id', 'name', 'type', 'times', 'starts2', 'ends2', 'eaten']
         empty_text = 'No meals available'
         order_by = 'starts'
 
@@ -44,11 +46,12 @@ class MealsUsersFilter(django_filters.FilterSet):
 
 
 class MealsUsersTable(tables.Table):
+    time2 = tables.DateTimeColumn(accessor='time', verbose_name='Time', format='d/m/Y G:i')
 
     class Meta:
         model = Meal
         attrs = {'class': 'table table-hover'}
         template = 'templates/meals_users.html'
-        fields = ['id', 'meal', 'user', 'time']
+        fields = ['id', 'meal', 'user', 'time2']
         empty_text = 'No hacker has eaten yet'
         order_by = 'time'
