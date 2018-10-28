dc69d38c36b02e3a04afb24fdcf421a1bb082bc0
MartinStevko <mstevko10@gmail.com>
2018-10-28 21:21:54 +0100

    Ambassador form

--- applications/forms.py
+++ applications/forms.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.template.defaultfilters import filesizeformat
 from django.utils import timezone
 from form_utils.forms import BetterModelForm
+from django.core.exceptions import ObjectDoesNotExist
 
 from app.mixins import OverwriteOnlyModelFormMixin
 from app.utils import validate_url
@@ -40,6 +41,14 @@ class ApplicationForm(OverwriteOnlyModelFormMixin, BetterModelForm):
                              help_text='Most recent degree you\'ve received',
                              widget=forms.TextInput(
                                  attrs={'class': 'typeahead-degrees', 'autocomplete': 'off'}))
+
+    ambassador = forms.CharField(
+        required=False,
+        label='What is your ambassador\'s secret code?',
+        help_text='Entering a secret code can help your ambassador. If you don\'t have one, \
+        leave this blank',
+        widget=forms.TextInput(attrs={'autocomplete': 'off'})
+    )
 
     first_timer = forms.TypedChoiceField(
         required=True,
@@ -132,8 +141,8 @@ class ApplicationForm(OverwriteOnlyModelFormMixin, BetterModelForm):
                 "To attend %s you must give us permission to shre your application" % settings.HACKATHON_NAME)
         return aps
 
-    def clean_media_parmission(self):
-        mp = self.cleaned_data.get('media_parmission', False)
+    def clean_media_permission(self):
+        mp = self.cleaned_data.get('media_permission', False)
         # Check if hackers give us permission to publish media files asociated with them
         if not mp and not self.instance.pk:
             raise forms.ValidationError(
@@ -201,6 +210,19 @@ class ApplicationForm(OverwriteOnlyModelFormMixin, BetterModelForm):
             raise forms.ValidationError("Please specify your race/ethnicity")
         return data
 
+    def clean_ambassador(self):
+        data = self.cleaned_data['ambassador']
+        if data:
+            try:
+                ambassador = models.Ambassador.objects.get(secret_code=data)
+            except(models.Ambassador.DoesNotExist):
+                raise forms.ValidationError("Ambassador with this secret code does not exist. \
+                Leave blank instead if you don't have ambassador")
+            else:
+                return ambassador
+        else:
+            return None
+
     def __getitem__(self, name):
         item = super(ApplicationForm, self).__getitem__(name)
         item.field.disabled = not self.instance.can_be_edit()
@@ -248,15 +270,17 @@ class ApplicationForm(OverwriteOnlyModelFormMixin, BetterModelForm):
         # Fields that we only need the first time the hacker fills the application
         # https://stackoverflow.com/questions/9704067/test-if-django-modelform-has-instance
         if not self.instance.pk:
-            self._fieldsets.append(('Permissions', 
-                {'fields': (
-                        'code_conduct',
-                        'privacy_policy',
-                        'application_sharing',
-                        'media_permission',
-                    ),
-                'description': 'We need this permissions to provide you better experiences'}
-            ))
+            self._fieldsets.append(('Secret code', {'fields': ('ambassador',),}))
+            self._fieldsets.append(
+                ('Permissions',
+                    {'fields': (
+                            'code_conduct',
+                            'privacy_policy',
+                            'application_sharing',
+                            'media_permission',
+                        ),
+                        'description': 'We need this permissions to provide you better experiences'}),
+            )
         return super(ApplicationForm, self).fieldsets
 
     class Meta:
@@ -303,7 +327,65 @@ class ApplicationForm(OverwriteOnlyModelFormMixin, BetterModelForm):
             'resume': 'Upload your resume',
             'reimb_amount': 'How much money (%s) would you need to afford traveling to %s?' % (
                 getattr(settings, 'CURRENCY', '$'), settings.HACKATHON_NAME),
-
         }
 
         exclude = ['user', 'uuid', 'invited_by', 'submission_date', 'status_update_date', 'status', 'under_age']
+
+
+class AmbassadorForm(OverwriteOnlyModelFormMixin, BetterModelForm):
+    university = forms.CharField(required=True,
+                                 label='What university do you study at?',
+                                 help_text='Current or most recent school you attended.',
+                                 widget=forms.TextInput(
+                                     attrs={'class': 'typeahead-schools', 'autocomplete': 'off'}))
+
+    privacy_policy = forms.BooleanField(required=False,
+                                      label='I have read and accept '
+                                            '<a href="%s" target="_blank">%s Privacy Policy</a>' % (
+                                                getattr(settings, 'PRIVACY_POLICY_LINK', '/privacy_policy'),
+                                                settings.HACKATHON_NAME), )
+
+    def clean_privacy_policy(self):
+        pc = self.cleaned_data.get('privacy_policy', False)
+        # Check that if it's the first submission hackers checks privacy policy checkbox
+        # self.instance.pk is None if there's no Application existing before
+        # https://stackoverflow.com/questions/9704067/test-if-django-modelform-has-instance
+        if not pc and not self.instance.pk:
+            raise forms.ValidationError(
+                "To attend %s you must abide by our privacy policy" % settings.HACKATHON_NAME)
+        return pc
+
+    def fieldsets(self):
+        self._fieldsets = [
+            ('Personal Info',
+             {'fields': ('origin', 'phone_number', 'university', 'tshirt_size'),
+             'description': 'Hey there, before we begin, we need to know some basics about you.', }),
+        ]
+
+        # Fields that we only need the first time the hacker fills the application
+        # https://stackoverflow.com/questions/9704067/test-if-django-modelform-has-instance
+        if not self.instance.pk:
+            self._fieldsets.append(('Permissions', 
+                {'fields': (
+                        'privacy_policy',
+                    ),
+                'description': 'We need this permissions to make you our\'s ambassador'}
+            ))
+
+        return super(AmbassadorForm, self).fieldsets
+
+    class Meta:
+        model = models.Ambassador
+
+        widgets = {
+            'origin': forms.TextInput(attrs={'autocomplete': 'off'}),
+            'tshirt_size': forms.RadioSelect(),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '+#########'})
+        }
+
+        labels = {
+            'origin': 'Where are you joining us from?',
+            'tshirt_size': 'What\'s your t-shirt size?',
+        }
+
+        exclude = ['user', 'secret_code', 'created_date']
