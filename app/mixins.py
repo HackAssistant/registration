454be7114f59a9a97107695cbbbeb81f1647c5ce
Gerard Casas Saez <casassg@gmail.com>
2018-11-05 12:00:53 -0700

    allow for a flexible action and method on table views

--- app/mixins.py
+++ app/mixins.py
@@ -10,7 +10,7 @@ class TabsViewMixin(object):
 
     def get_context_data(self, **kwargs):
         c = super(TabsViewMixin, self).get_context_data(**kwargs)
-        c.update({'tabs': self.get_current_tabs(), 'back': self.get_back_url()})
+        c.update({'tabs': self.get_current_tabs(), 'back': self.get_back_url(), 'form_method': 'post'})
         return c
 
 
