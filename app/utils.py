5f14d02a6a2ae51977cf75fc27e8be775aa3e135
Gerard Casas Saez <casassg@gmail.com>
2018-11-07 13:41:59 -0700

    add variable to toggle if hackers can request items on hwlab

--- app/utils.py
+++ app/utils.py
@@ -86,6 +86,7 @@ def get_substitutions_templates():
             'h_r_days': getattr(settings, 'REIMBURSEMENT_EXPIRY_DAYS', None),
             'h_r_enabled': getattr(settings, 'REIMBURSEMENT_ENABLED', False),
             'h_hw_enabled': getattr(settings, 'HARDWARE_ENABLED', False),
+            'h_hw_hacker_request': getattr(settings, 'HACKERS_CAN_REQUEST', True),
             }
 
 
