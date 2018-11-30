2d34090b5676bfc19adb0705bd9b058bfe11359c
Juraj Mičko <micko.juraj64@gmail.com>
2018-11-30 02:24:14 +0000

    HK: fix typo

--- applications/forms.py
+++ applications/forms.py
@@ -468,7 +468,7 @@ class ApplicationForm(OverwriteOnlyModelFormMixin, BetterModelForm):
             'graduation_year': 'What year have you graduated on or when will '
                                'you graduate',
             'degree': 'What\'s your major?',
-            'birth_day': 'When you click on a field, you should see a pop-up where you can \
+            'birth_day': 'When you click on the field, you should see a pop-up where you can \
             choose a date.',
             'other_diet': 'Please fill here in your dietary requirements. We want to make sure we have food for you!',
             'projects': 'You can talk about about past hackathons, personal projects, awards etc. '
