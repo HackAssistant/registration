28a60ea7328d8caeccdb786621a26fb675294fd4
MartinStevko <mstevko10@gmail.com>
2018-12-16 18:26:25 +0100

    HK: Visa invitation letter email message

--- applications/emails.py
+++ applications/emails.py
@@ -9,6 +9,7 @@ def create_invite_email(application, request):
     c = {
         'name': application.user.get_full_name,
         'reimb': getattr(application.user, 'reimbursement', None),
+        'visas': application.visas,
         'confirm_url': str(reverse('confirm_app', request=request, kwargs={'id': application.uuid_str})),
         'cancel_url': str(reverse('cancel_app', request=request, kwargs={'id': application.uuid_str}))
     }
