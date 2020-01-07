2aeba2e8c4814db3f996ccd1b9b0687a87c1bb7f
jjurm <micko.juraj64@gmail.com>
2020-01-07 23:36:07 +0100

    Improve email templates

--- app/hackathon_variables.py
+++ app/hackathon_variables.py
@@ -37,11 +37,11 @@ HACKATHON_APP_OPENING = timezone.datetime(2019, 11, 15, 18, 00, tzinfo=timezone.
 HACKATHON_APP_DEADLINE = timezone.datetime(2020, 3, 13, 0, 0, tzinfo=timezone.pytz.timezone(TIME_ZONE))
 # (OPTIONAL) When to arrive at the hackathon
 # Registration and Closing ceremony times (make sure we won't need to change them)
-HACKATHON_ARRIVE = 'Registration opens at <b>9:00</b> and closes at 9:30 on Saturday, <b>28 March</b>, ' \
-                   'the opening ceremony will be at 10:00.'
+HACKATHON_ARRIVE = 'Registration opens at 9:00 and closes at 9:30 on Saturday, 28 March, ' \
+                   'the opening ceremony starts at 10:00.'
 
 # (OPTIONAL) When to arrive at the hackathon
-HACKATHON_LEAVE = 'Closing ceremony will be held on Sunday, <b>29 March</b> and is expected to finish by 18.00. ' \
+HACKATHON_LEAVE = 'Closing ceremony will be held on Sunday, 29 March and is expected to finish by 18.00. ' \
                   'The projects demo fair will be held before, on Sunday afternoon.'
 # (OPTIONAL) Hackathon live page
 HACKATHON_LIVE_PAGE = 'https://hackkosice.com/'
