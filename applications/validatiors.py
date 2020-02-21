d9b16cace49fb469a70ebe658d5ad19b60c72282
Álvaro Girona Arias <alvaro.girona@gmail.com>
2020-02-21 13:58:18 +0100

    Moved supported resume file extensions to hackathon_variables

--- applications/validatiors.py
+++ applications/validatiors.py
@@ -1,8 +1,9 @@
-
 def validate_file_extension(value):
     import os
     from django.core.exceptions import ValidationError
-    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
-    valid_extensions = ['.pdf']
+    from django.conf import settings
+    (_, ext) = os.path.splitext(value.name)
+
+    valid_extensions = settings.SUPPORTED_RESUME_EXTENSIONS
     if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file extension.')
