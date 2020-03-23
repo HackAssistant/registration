17aa2656a58e16d62dfad1b6a0e7e1382535bf03
casassarnau <33383702+Casassarnau@users.noreply.github.com>
2020-03-24 00:46:12 +0100

    Signup redirected

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ from app import utils, mixins
 
 def root_view(request):
     if not request.user.is_authenticated() and not utils.is_app_closed():
-        return HttpResponseRedirect(reverse('account_signup', kwargs={'u_type': 'hacker'}))
+        return HttpResponseRedirect(reverse('account_signup'))
     if not request.user.is_authenticated() and utils.is_app_closed():
         return HttpResponseRedirect(reverse('account_login'))
     if not request.user.has_usable_password():
