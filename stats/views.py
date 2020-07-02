5bf570bb413a4f08ad6e2bfaa1e6c13b9102c37c
casassarnau <casassarnau@github.com>
2020-07-02 19:09:48 +0200

    mentor stats & fixes

--- stats/views.py
+++ stats/views.py
@@ -17,10 +17,150 @@ from collections import defaultdict
 
 def stats_tabs():
     tabs = [('Applications', reverse('app_stats'), False), ('Users', reverse('users_stats'), False),
-            ('Check-in', reverse('checkin_stats'), False), ('Volunteer', reverse('volunteer_stats'), False)]
+            ('Check-in', reverse('checkin_stats'), False), ('Volunteer', reverse('volunteer_stats'), False),
+            ('Mentor', reverse('mentor_stats'), False)]
     if getattr(settings, 'REIMBURSEMENT_ENABLED', False):
         tabs.append(('Reimbursements', reverse('reimb_stats'), False))
     return tabs
+
+
+def get_stats(model):
+    hacker = model == HackerApplication
+    volunteer = model == VolunteerApplication
+    mentor = model == MentorApplication
+    sponsor = model == SponsorApplication
+    # Status analysis
+    tshirt_dict = dict(TSHIRT_SIZES)
+    diets_dict = dict(DIETS)
+    applications = list(model.objects.all())
+    status_count = defaultdict(int)
+    gender_count = defaultdict(int)
+    origin_count = defaultdict(int)
+    origin_count_confirmed = defaultdict(int)
+    university_count = defaultdict(int)
+    university_count_confirmed = defaultdict(int)
+    grad_year_count = defaultdict(int)
+    grad_year_count_confirmed = defaultdict(int)
+    degree_count = defaultdict(int)
+    degree_count_confirmed = defaultdict(int)
+    first_timer_count = defaultdict(int)
+    first_timer_count_confirmed = defaultdict(int)
+    shirt_count = defaultdict(int)
+    shirt_count_confirmed = defaultdict(int)
+    diet_count = defaultdict(int)
+    diet_count_confirmed = defaultdict(int)
+    lennyface_count = defaultdict(int)
+    lennyface_count_confirmed = defaultdict(int)
+    resume_count = defaultdict(int)
+    resume_count_confirmed = defaultdict(int)
+    study_work_count = defaultdict(int)
+    study_work_count_confirmed = defaultdict(int)
+    other_diet = []
+    for a in applications:
+        status_count[a.get_status_display()] += 1
+        shirt_count[tshirt_dict[a.tshirt_size]] += 1
+        diet_count[diets_dict[a.diet]] += 1
+        if hacker or volunteer or mentor:
+            origin_count[a.origin] += 1
+            gender_count[a.get_gender_display()] += 1
+        if hacker or volunteer:
+            university_count[a.university] += 1
+            grad_year_count[a.graduation_year] += 1
+            degree_count[a.degree] += 1
+            first_timer_count[a.first_timer] += 1
+        if hacker:
+            lennyface_count[a.lennyface] += 1
+            resume_count['Resume'] += a.resume != ""
+            resume_count['No resume'] += a.resume == ""
+        if mentor and a.study_work:
+            study_work_count['Student'] += 1
+        elif mentor:
+            study_work_count['Worker'] += 1
+
+        if a.status == APP_CONFIRMED:
+            shirt_count_confirmed[tshirt_dict[a.tshirt_size]] += 1
+            diet_count_confirmed[diets_dict[a.diet]] += 1
+            if hacker or  volunteer or mentor:
+                origin_count_confirmed[a.origin] += 1
+            if hacker or volunteer:
+                university_count_confirmed[a.university] += 1
+                grad_year_count_confirmed[a.graduation_year] += 1
+                degree_count_confirmed[a.degree] += 1
+                first_timer_count_confirmed[a.first_timer] += 1
+            if hacker:
+                lennyface_count_confirmed[a.lennyface] += 1
+                resume_count_confirmed['Resume'] += a.resume != ""
+                resume_count_confirmed['No resume'] += a.resume == ""
+            if mentor and a.study_work:
+                study_work_count_confirmed['Student'] += 1
+            elif mentor:
+                study_work_count_confirmed['Worker'] += 1
+            if a.diet == D_OTHER and a.other_diet:
+                other_diet.append(a.other_diet)
+    
+    shirt_count = [{'tshirt': x, 'applications': v} for (x, v) in shirt_count.items()]
+    shirt_count_confirmed = [{'tshirt': x, 'applications': v} for (x, v) in shirt_count_confirmed.items()]
+
+    diet_count = [{'diet': x, 'applications': v} for (x, v) in diet_count.items()]
+    diet_count_confirmed = [{'diet': x, 'applications': v} for (x, v) in diet_count_confirmed.items()]
+
+    if hacker or volunteer or mentor:
+        origin_count = [{'origin': x, 'applications': v} for (x, v) in sorted(origin_count.items(),
+                                                                            key=lambda item: item[1])[-10:]]
+        origin_count_confirmed = [{'origin': x, 'applications': v} for (x, v) in sorted(origin_count_confirmed.items(),
+                                                                                        key=lambda item: item[1])[-10:]]
+
+    if hacker or volunteer:
+        university_count = [{'university': x, 'applications': v} for (x, v) in sorted(university_count.items(),
+                                                                                    key=lambda item: item[1])[-10:]]
+        university_count_confirmed = [{'university': x, 'applications': v} for (x, v) in
+                                    sorted(university_count_confirmed.items(), key=lambda item: item[1])[-10:]]
+
+        degree_count = [{'degree': x, 'applications': v} for (x, v) in
+                        sorted(degree_count.items(), key=lambda item: item[1])[-10:]]
+        degree_count_confirmed = [{'degree': x, 'applications': v} for (x, v) in
+                                sorted(degree_count_confirmed.items(), key=lambda item: item[1])[-10:]]
+
+    if hacker:
+        lennyface_count = [{'lennyface': x, 'applications': v} for (x, v) in
+                            sorted(lennyface_count.items(), key=lambda item: item[1])[-5:]]
+        lennyface_count_confirmed = [{'lennyface': x, 'applications': v} for (x, v) in
+                                    sorted(lennyface_count_confirmed.items(), key=lambda item: item[1])[-5:]]
+
+    timeseries = model.objects.all().annotate(date=TruncDate('submission_date')).values('date').annotate(
+        applications=Count('date'))
+
+    return JsonResponse(
+        {
+            'timeseries': list(timeseries),
+            'update_time': timezone.now(),
+            'app_count': len(applications),
+            'status': status_count,
+            'tshirt': shirt_count,
+            'tshirt_confirmed': shirt_count_confirmed,
+            'gender': gender_count,
+            'university': university_count,
+            'university_confirmed': university_count_confirmed,
+            'graduation_year': grad_year_count,
+            'graduation_year_confirmed': grad_year_count_confirmed,
+            'degree': degree_count,
+            'degree_confirmed': degree_count_confirmed,
+            'first_timer': first_timer_count,
+            'first_timer_confirmed': first_timer_count_confirmed,
+            'origin': origin_count,
+            'origin_confirmed': origin_count_confirmed,
+            'diet': diet_count,
+            'diet_confirmed': diet_count_confirmed,
+            'lennyface': lennyface_count,
+            'lennyface_confirmed': lennyface_count_confirmed,
+            'resume': resume_count,
+            'resume_confirmed': resume_count_confirmed,
+            'other_diet': '<br>'.join([el for el in other_diet]),
+            'study_work': study_work_count,
+            'study_work_confirmed': study_work_count_confirmed,
+        }
+    )
+
 
 
 @is_organizer
@@ -103,6 +243,17 @@ def checkin_stats_api(request):
     )
 
 
+@is_organizer
+def volunteer_stats_api(request):
+    return get_stats(VolunteerApplication)
+
+
+@is_organizer
+def mentor_stats_api(request):
+    return get_stats(MentorApplication)
+
+
+
 class AppStats(IsOrganizerMixin, TabsView):
     template_name = 'application_stats.html'
 
@@ -130,135 +281,16 @@ class CheckinStats(IsOrganizerMixin, TabsView):
     def get_current_tabs(self):
         return stats_tabs()
 
+
 class VolunteerStats(IsOrganizerMixin, TabsView):
     template_name = 'volunteer_stats.html'
 
     def get_current_tabs(self):
         return stats_tabs()
 
-@is_organizer
-def volunteer_stats_api(request):
-    return get_stats(VolunteerApplication)
 
+class MentorStats(IsOrganizerMixin, TabsView):
+    template_name = 'mentor_stats.html'
 
-def get_stats(model):
-    hacker = model == HackerApplication
-    volunteer = model == VolunteerApplication
-    mentor = model == MentorApplication
-    sponsor = model == SponsorApplication
-    # Status analysis
-    tshirt_dict = dict(TSHIRT_SIZES)
-    diets_dict = dict(DIETS)
-    applications = list(model.objects.all())
-    status_count = defaultdict(int)
-    gender_count = defaultdict(int)
-    origin_count = defaultdict(int)
-    origin_count_confirmed = defaultdict(int)
-    university_count = defaultdict(int)
-    university_count_confirmed = defaultdict(int)
-    grad_year_count = defaultdict(int)
-    grad_year_count_confirmed = defaultdict(int)
-    degree_count = defaultdict(int)
-    degree_count_confirmed = defaultdict(int)
-    first_timer_count = defaultdict(int)
-    first_timer_count_confirmed = defaultdict(int)
-    shirt_count = defaultdict(int)
-    shirt_count_confirmed = defaultdict(int)
-    diet_count = defaultdict(int)
-    diet_count_confirmed = defaultdict(int)
-    lennyface_count = defaultdict(int)
-    lennyface_count_confirmed = defaultdict(int)
-    resume_count = defaultdict(int)
-    resume_count_confirmed = defaultdict(int)
-    other_diet = []
-    for a in applications:
-        status_count[a.get_status_display()] += 1
-        shirt_count[tshirt_dict[a.tshirt_size]] += 1
-        diet_count[diets_dict[a.diet]] += 1
-        if hacker or volunteer:
-            gender_count[a.get_gender_display()] += 1
-            origin_count[a.origin] += 1
-            university_count[a.university] += 1
-            grad_year_count[a.graduation_year] += 1
-            degree_count[a.degree] += 1
-            first_timer_count[a.first_timer] += 1
-        if hacker:
-            lennyface_count[a.lennyface] += 1
-            resume_count['Resume'] += a.resume != ""
-            resume_count['No resume'] += a.resume == ""
-
-        if a.status == APP_CONFIRMED:
-            shirt_count_confirmed[tshirt_dict[a.tshirt_size]] += 1
-            diet_count_confirmed[diets_dict[a.diet]] += 1
-            if hacker or volunteer:
-                origin_count_confirmed[a.origin] += 1
-                university_count_confirmed[a.university] += 1
-                grad_year_count_confirmed[a.graduation_year] += 1
-                degree_count_confirmed[a.degree] += 1
-                first_timer_count_confirmed[a.first_timer] += 1
-            if hacker:
-                lennyface_count_confirmed[a.lennyface] += 1
-                resume_count_confirmed['Resume'] += a.resume != ""
-                resume_count_confirmed['No resume'] += a.resume == ""
-            if a.diet == D_OTHER and a.other_diet:
-                other_diet.append(a.other_diet)
-    
-    shirt_count = [{'tshirt': x, 'applications': v} for (x, v) in shirt_count.items()]
-    shirt_count_confirmed = [{'tshirt': x, 'applications': v} for (x, v) in shirt_count_confirmed.items()]
-
-    diet_count = [{'diet': x, 'applications': v} for (x, v) in diet_count.items()]
-    diet_count_confirmed = [{'diet': x, 'applications': v} for (x, v) in diet_count_confirmed.items()]
-
-    if hacker or volunteer:
-        origin_count = [{'origin': x, 'applications': v} for (x, v) in sorted(origin_count.items(),
-                                                                            key=lambda item: item[1])[-10:]]
-        origin_count_confirmed = [{'origin': x, 'applications': v} for (x, v) in sorted(origin_count_confirmed.items(),
-                                                                                        key=lambda item: item[1])[-10:]]
-
-        university_count = [{'university': x, 'applications': v} for (x, v) in sorted(university_count.items(),
-                                                                                    key=lambda item: item[1])[-10:]]
-        university_count_confirmed = [{'university': x, 'applications': v} for (x, v) in
-                                    sorted(university_count_confirmed.items(), key=lambda item: item[1])[-10:]]
-
-        degree_count = [{'degree': x, 'applications': v} for (x, v) in
-                        sorted(degree_count.items(), key=lambda item: item[1])[-10:]]
-        degree_count_confirmed = [{'degree': x, 'applications': v} for (x, v) in
-                                sorted(degree_count_confirmed.items(), key=lambda item: item[1])[-10:]]
-
-    if hacker:
-        lennyface_count = [{'lennyface': x, 'applications': v} for (x, v) in
-                            sorted(lennyface_count.items(), key=lambda item: item[1])[-5:]]
-        lennyface_count_confirmed = [{'lennyface': x, 'applications': v} for (x, v) in
-                                    sorted(lennyface_count_confirmed.items(), key=lambda item: item[1])[-5:]]
-
-    timeseries = model.objects.all().annotate(date=TruncDate('submission_date')).values('date').annotate(
-        applications=Count('date'))
-
-    return JsonResponse(
-        {
-            'timeseries': list(timeseries),
-            'update_time': timezone.now(),
-            'app_count': len(applications),
-            'status': status_count,
-            'tshirt': shirt_count,
-            'tshirt_confirmed': shirt_count_confirmed,
-            'gender': gender_count,
-            'university': university_count,
-            'university_confirmed': university_count_confirmed,
-            'graduation_year': grad_year_count,
-            'graduation_year_confirmed': grad_year_count_confirmed,
-            'degree': degree_count,
-            'degree_confirmed': degree_count_confirmed,
-            'first_timer': first_timer_count,
-            'first_timer_confirmed': first_timer_count_confirmed,
-            'origin': origin_count,
-            'origin_confirmed': origin_count_confirmed,
-            'diet': diet_count,
-            'diet_confirmed': diet_count_confirmed,
-            'lennyface': lennyface_count,
-            'lennyface_confirmed': lennyface_count_confirmed,
-            'resume': resume_count,
-            'resume_confirmed': resume_count_confirmed,
-            'other_diet': '<br>'.join([el for el in other_diet])
-        }
-    )
+    def get_current_tabs(self):
+        return stats_tabs()
