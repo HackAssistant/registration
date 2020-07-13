01c2e73fc4bc8b9190d75b6ea9622b92fd42610f
casassarnau <33383702+Casassarnau@users.noreply.github.com>
2020-07-13 19:29:20 +0200

    migration fix

--- user/migrations/0010_blacklist_and_user_type_permisions_20200520_1452.py
+++ user/migrations/0010_blacklist_and_user_type_permisions_20200520_1452.py
@@ -8,25 +8,11 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('user', '0009_auto_20200426_0854')
         ('user', '0009_user_types_20200321_0441'),
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='BlacklistUser',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('email', models.EmailField(max_length=255, unique=True, verbose_name='email')),
-                ('name', models.CharField(max_length=255, verbose_name='Full name')),
-                ('motive_of_ban', models.CharField(blank=True, max_length=300, null=True)),
-                ('date_of_ban', models.DateTimeField()),
-            ],
-        ),
-        migrations.AddField(
-            model_name='user',
-            name='can_review_blacklist',
-            field=models.BooleanField(default=False),
-        ),
         migrations.AddField(
             model_name='user',
             name='can_review_mentors',
