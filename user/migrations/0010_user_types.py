b6bffa6a9909722b136a83bfa81ca9de2f8acd0b
casassarnau <33383702+Casassarnau@users.noreply.github.com>
2020-07-13 19:16:10 +0200

    migration solved on heroku

--- user/migrations/0010_user_types.py
+++ user/migrations/0010_user_types.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user', '0009_blacklist'),
+        ('user', '0009_auto_20200426_0854'),
     ]
 
     operations = [
