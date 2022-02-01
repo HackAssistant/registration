474739e2a21210344c23ac6f6c6fce4f436fe719
EncryptEx <41539618+EncryptEx@users.noreply.github.com>
2022-02-01 21:05:46 +0100

    Added fade in and fade out to make more intuitive. Made all requests with status 200

--- meals/views.py
+++ meals/views.py
@@ -181,25 +181,25 @@ class MealsCoolAPI(View, IsVolunteerMixin):
         qrid = request.POST.get('qr_id', None)
 
         if not qrid or not mealid:
-            return JsonResponse({'error': 'Missing meal and/or QR. Trying to trick us?'}, status=400)
+            return JsonResponse({'error': 'Missing meal and/or QR. Trying to trick us?', 'success' : False})
 
         current_meal = Meal.objects.filter(id=mealid).first()
         if not current_meal.opened and not self.request.user.is_organizer:
-            return JsonResponse({'error': 'Meal has been closed. Reach out to an organizer to activate it again'}, status=400)
+            return JsonResponse({'error': 'Meal has been closed. Reach out to an organizer to activate it again', 'success' : False})
         hacker_checkin = CheckIn.objects.filter(qr_identifier=qrid).first()
         if not hacker_checkin:
-            return JsonResponse({'error': 'Invalid QR code!'}, status=400)
+            return JsonResponse({'error': 'Invalid QR code!', 'success' : False})
 
         hacker_application = hacker_checkin.application
         if not hacker_application:
-            return JsonResponse({'error': 'No application found for current code'},  status=400)
+            return JsonResponse({'error': 'No application found for current code', 'success' : False})
 
         times_hacker_ate = Eaten.objects.filter(meal=current_meal, user=hacker_application.user).count()
         if times_hacker_ate >= current_meal.times:
             error_message = 'Warning! Hacker already ate %d out of %d available times!' % \
                             (times_hacker_ate, current_meal.times)
 
-            return JsonResponse({'error': error_message},  status=400)
+            return JsonResponse({'error': error_message, 'success' : False})
 
         checkin = Eaten(meal=current_meal, user=hacker_application.user)
         checkin.save()
@@ -211,7 +211,7 @@ class MealsCoolAPI(View, IsVolunteerMixin):
         else:
             diet = hacker_application.diet
 
-        return JsonResponse({'diet': diet},  status=200)
+        return JsonResponse({'diet': diet, 'success':True})
 
 
 class MealSerializer(Serializer):
