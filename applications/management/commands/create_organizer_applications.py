09a94a8307a2df8ade26a788166924f3320484b9
Arnau Casas <33383702+Casassarnau@users.noreply.github.com>
2023-05-08 14:00:03 +0200

    Fixed bug organizer

--- applications/management/commands/create_organizer_applications.py
+++ applications/management/commands/create_organizer_applications.py
@@ -7,7 +7,13 @@ from user.models import User, USR_ORGANIZER
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        user = User.objects.create_sponsor(email='organizer@organizer.com', name='Organizers')
+        if not User.objects.filter(email='organizer@organizer.com').exists():
+            User.objects.create_sponsor(email='organizer@organizer.com', name='Organizers')
+        i = 0
         organizers = User.objects.filter(type=USR_ORGANIZER)
         for organizer in organizers:
+            while User.objects.filter(email='organizer+%s@organizer.com' % i).exists():
+                i += 1
+            user = User.objects.create_sponsor(email='organizer+%s@organizer.com' % i, name=organizer.name)
             SponsorApplication(name=organizer.name, user=user).save()
+            i += 1
